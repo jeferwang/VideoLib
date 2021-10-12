8d54cf96265eac93065bc1bb5fc6c35d8a98100d
jefer <1737723363@qq.com>
2021-10-12 22:30:25 +0800

    modify

--- Test.cpp
+++ Test.cpp
@@ -5,23 +5,23 @@
 #include "lodepng.h"
 
 int main() {
-    // add a LoggerWriter
-    auto ConsoleWriter = new VP::Logger::ConsoleLogWriter();
-    VP::Logger::Log::RegisterLogger(ConsoleWriter);
-    // init VideoPlayer
-    std::string Url = "D:/Projects/XDance1.0/Client/Assets/Resources/DanceVideos/jiuzuidehudiezuo/jiuzuidehudiezuo.mp4";
-    VP::Player P;
-    P.Init(Url.c_str());
-    P.SetFrameUpdateCallback([&P](void *ImageData, int32_t FrameIndex, int64_t FramePTS, int32_t Width, int32_t Height, int32_t LineSize) {
-        // Save frame rgba to png
-        // char FileName[256] = {'\0'};
-        // std::snprintf(FileName, 256, "D:/Projects/VPLib/Frame/%d.png", FrameIndex);
-        // unsigned error = lodepng_encode32_file(FileName, (uint8_t *) ImageData, P.GetWidth(), P.GetHeight());
-        // VP::Logger::Log::Write(VP::Logger::LogLevel::Info, "lodepng_encode32_file error=%d", error);
-    });
-    P.Play();
-    std::this_thread::sleep_for(std::chrono::seconds (1));
-    P.UnInit();
-    std::this_thread::sleep_for(std::chrono::seconds (3));
-    return 0;
+	// add a LoggerWriter
+	auto ConsoleWriter = new VP::Logger::ConsoleLogWriter();
+	VP::Logger::Log::RegisterLogger(ConsoleWriter);
+	// init VideoPlayer
+	std::string Url = "D:/Projects/XDance1.0/Client/Assets/Resources/DanceVideos/jiuzuidehudiezuo/jiuzuidehudiezuo.mp4";
+	VP::Player P;
+	P.Init(Url.c_str());
+	P.SetFrameUpdateCallback([&P](void* ImageData, int32_t FrameIndex, int64_t FramePTS, int32_t Width, int32_t Height, int32_t LineSize) {
+		//Save frame rgba to png
+		//char FileName[256] = {'\0'};
+		//std::snprintf(FileName, 256, "D:/Projects/VPLib/Frame/%d.png", FrameIndex);
+		//unsigned error = lodepng_encode32_file(FileName, (uint8_t *) ImageData, P.GetWidth(), P.GetHeight());
+		//VP::Logger::Log::Write(VP::Logger::LogLevel::Info, "lodepng_encode32_file error=%d", error);
+		});
+	P.Play();
+	std::this_thread::sleep_for(std::chrono::seconds(1));
+	P.UnInit();
+	std::this_thread::sleep_for(std::chrono::seconds(3));
+	return 0;
 }
