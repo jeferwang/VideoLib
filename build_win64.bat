7c66e86a8102920767774a1e4f47bac4d18b5893
jefer <1737723363@qq.com>
2021-11-20 20:22:56 +0800

    modify project name

--- build_win64.bat
+++ build_win64.bat
@@ -4,5 +4,5 @@ set BUILD_DIR="build/win64"
 
 rmdir /s /q %BUILD_DIR%
 cmake -B %BUILD_DIR% -S . -A x64 -G "Visual Studio 16 2019"
-cmake --build %BUILD_DIR% --target VideoPlayerCore --config Debug
-@REM cmake --build %BUILD_DIR% --target VideoPlayerCore --config Release
+cmake --build %BUILD_DIR% --target XVideo --config Debug
+@REM cmake --build %BUILD_DIR% --target XVideo --config Release
